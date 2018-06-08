62330e344fa0e0ed98e9d6e59c3de2734faa4350
Isaac Pierce <contact@isaacdpierce.com>
2018-06-07 17:52:51 -0700

    Implement search input, pagination and buttons UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,50 @@
-import str from './models/Search';
-import { add, multiply, ID } from './views/searchView'
+import Search from './models/Search';
+import * as searchView from './views/searchView'
+import { elements, renderLoader, clearLoader } from './views/base';
 
-console.log(`Using imported functions to add ${add(ID, 2)} and ${multiply(9, 6)} and ${str}  `);
+// Global State of App
+// * Search Object
+// * Current Recipe Object
+// * Shopping list object
+// * Liked recipes
 
+const state = {};
 
+const controlSearch = async () => {
+  // 1) Get query
+  const query = searchView.getInput(); // TODO
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+    // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
+    // 4) search for recipes
+    await state.search.getResults();
+    // 5) Render results on UI
+    clearLoader();
+    searchView.renderResults(state.search.result);
+    
+
+  }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', (e) => {
+  const btn = e.target.closest('.btn-inline');
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+    console.log(goToPage);
+    
+  }
+  
+});
